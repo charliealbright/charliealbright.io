9eee40e7fe6a8b27377c317ef9db41b904efd4e2
Charlie Albright <charlie.albright@bottlerocketstudios.com>
2017-03-15 00:58:55 -0500

    Built initial Gulp build tasks, got Sass working, etc.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/images'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -14,7 +15,5 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', ['nodemon'], function() {
+    browserSync.init({
+        proxy: 'localhost:5000',
+        port: 3000,
+        notify: true
+    })
+})
+
+gulp.task('nodemon', function () {
+    nodemon(
+        {
+            script: 'app/index.js',
+            ignore: [
+                'gulpfile.js',
+                'node_modules/'
+            ]
+        }
+    ).on('restart', function () {
+      console.log('restarted!')
+    })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
